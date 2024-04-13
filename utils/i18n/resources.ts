4ab5ed5967de9502ff8b6d8ccff9fc198f6f57a3
Tim Vucina <tim.vucina@gmail.com>
2024-04-13 14:46:19 +0200

    moved i18n folder to utils

--- utils/i18n/resources.ts
+++ utils/i18n/resources.ts
@@ -2,8 +2,8 @@
 // Translations are located in /locales/{language}/{namespace}.ts
 
 // Import namespaces for each language.
-import commonEn from '../locales/en/common';
-import commonSl from '../locales/sl/common';
+import commonEn from '../../locales/en/common';
+import commonSl from '../../locales/sl/common';
 
 const resources = {
   // Define namespaces for each language.
